31159a95854fec7205468e50917a507632a706d0
leduc1901 <letrongduc1999@gmail.com>
2020-06-05 12:12:27 +0700

    updating

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.7.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.1'
+gem 'bootstrap-sass', '~>3'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', '~> 1.4'
 # Use Puma as the app server
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'user/new'
+  root 'static_pages#home'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup' => 'user#new'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
